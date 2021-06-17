4e2865dc510930ead2e1c0f39d53669d1e268269
Praveen Kumar <pk1798.kumar@gmail.com>
2021-06-17 17:06:29 +0530

    optitimized images

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,22 +1,46 @@
 const burgerIcon = document.getElementById('burger-icon');
 const navbar = document.getElementById('navbar');
 
-burgerIcon.addEventListener('click', function(){
+burgerIcon.addEventListener('mouseover', function(){
     navbar.classList.toggle('show');
 });
 
+
 const dropdownMenuMain= document.getElementById('dropdownMenuMain');
 const dropdownContentMain = document.getElementById('dropdownContentMain');
+const upMain = document.getElementById('up-main');
+const downMain = document.getElementById('down-main');
+
 
 dropdownMenuMain.addEventListener('click' ,function(){
     dropdownContentMain.classList.toggle('hide');
+    upMain.classList.toggle('hide');
+    downMain.classList.toggle('hide');
 });
 
 
 const dropdownMenuSecondary= document.getElementById('dropdownMenuSecondary');
 const dropdownContentSecondary = document.getElementById('dropdownContentSecondary');
+const upSecondary = document.getElementById('up-secondary');
+const downSecondary = document.getElementById('down-secondary');
 
 dropdownMenuSecondary.addEventListener('click' ,function(){
     dropdownContentSecondary.classList.toggle('hide');
+    upSecondary.classList.toggle('hide');
+    downSecondary.classList.toggle('hide');
 });
 
+const countryValue = document.getElementById('country-value');
+const india = document.getElementById('india');
+const china = document.getElementById('china');
+const japan = document.getElementById('japan');
+
+india.addEventListener('click', function(){
+    countryValue.innerText = india.innerText
+})
+china.addEventListener('click', function(){
+    countryValue.innerText = china.innerText
+})
+japan.addEventListener('click', function(){
+    countryValue.innerText = japan.innerText
+})
--- src/js/smallCard.js
+++ src/js/smallCard.js
@@ -1,69 +1,3 @@
-const smallCard = [{
-    genre:'Action',
-    Image : './img/knight.png',
-    figcaption: 'Valorant',
-    description: 'Deal 5000 damage to enemies with grenades.',
-    uproarPoints: '+100',
-    boltPoints: '-100',
-
-},
-{
-    genre:'Adventure',
-    Image : './img/knight.png',
-    figcaption: 'Valorant',
-    description: 'Deal 5000 damage to enemies with grenades.',
-    uproarPoints: '+100',
-    boltPoints: '-100',
-
-},
-{
-    genre:'Casual',
-    Image : './img/knight.png',
-    figcaption: 'Valorant',
-    description: 'Deal 5000 damage to enemies with grenades.',
-    uproarPoints: '+100',
-    boltPoints: '-100',
-
-},
-{
-    genre:'Intellectual',
-    Image : './img/knight.png',
-    figcaption: 'Valorant',
-    description: 'Deal 5000 damage to enemies with grenades.',
-    uproarPoints: '+100',
-    boltPoints: '-100',
-
-},
-{
-    genre:'Intellectual',
-    Image : './img/knight.png',
-    figcaption: 'Valorant',
-    description: 'Deal 5000 damage to enemies with grenades.',
-    uproarPoints: '+100',
-    boltPoints: '-100',
-
-},
-{
-    genre:'Strategy',
-    Image : './img/knight.png',
-    figcaption: 'Valorant',
-    description: 'Deal 5000 damage to enemies with grenades.',
-    uproarPoints: '+100',
-    boltPoints: '-100',
-
-},
-{
-    genre:'Sport',
-    Image : './img/knight.png',
-    figcaption: 'Valorant',
-    description: 'Deal 5000 damage to enemies with grenades.',
-    uproarPoints: '+100',
-    boltPoints: '-100',
-
-}];
-
-
-
 //To list cards.
 
 
@@ -76,26 +10,31 @@ function listSmallCard(arr){
         li.className = 'glide__slide';
         smallContainer.appendChild(li)
     
-        var card = `<div class="card-small">
+        var card = `
+        <div class="card-wrapper">
+        <div class="card-small">
         <figure>
             <p class="genre">${card.genre}</p>
-            <img src="${card.Image}" alt="">
+            <img src="${card.image}"
+            alt="${card.name}"
+            srcset="${card.image} 1x, ${card.image2x} 2x,${card.image3x} 3x" >
             <figcaption>
-                <h3> Star Knight </h3>
-                <p>Earn 10,000 сoins.</p>
+                <h3> ${card.figcaption} </h3>
+                <p>${card.description}</p>
             </figcaption>
         </figure>
         <div class="card__points">
             <div class="points">
                 <svg xmlns="http://www.w3.org/2000/svg" class="logo" width="13" height="12"><path fill="#FFF" d="M11.806 3.678a2.53 2.53 0 00-.726-.28l.021-.074c.09-.34.363-1.374-.273-2.23-.461-.62-1.162-.81-1.537-.911C8.979.098 8.565 0 8.132 0 7.481 0 6.923.228 6.5.631 6.077.228 5.52 0 4.87 0c-.434 0-.847.098-1.157.182-.377.102-1.078.292-1.54.913-.635.856-.362 1.889-.273 2.23l.021.074a2.548 2.548 0 00-.727.28c-1.257.736-1.56 2.342-.72 3.818.54.95 1.719 1.507 2.773 1.646C3.43 10.493 4.854 11.999 6.5 12c1.647 0 3.072-1.506 3.255-2.857 1.053-.139 2.234-.696 2.773-1.646.84-1.477.537-3.082-.721-3.819zM7.129 2.245c.269-1.017 1.075-.896 1.8-.698.725.196 1.094.394.826 1.412-.27 1.017-1.075 2.47-1.8 2.272-.725-.197-1.095-1.969-.826-2.986zM4.07 1.546c.725-.198 1.531-.318 1.8.698.268 1.018-.1 2.79-.826 2.987-.725.197-1.53-1.256-1.8-2.273-.269-1.017.1-1.215.826-1.412zm.262 6.098C3.756 7.98 2.139 7.6 1.68 6.79c-.46-.808-.367-1.552.21-1.89.576-.336 1.415-.138 1.874.671.461.807 1.145 1.736.569 2.072zM6.5 10.587c-1.04 0-1.882-1.124-1.882-1.732 0-.608.843-.472 1.882-.472s1.881-.136 1.881.472-.842 1.732-1.881 1.732zm4.82-3.796c-.46.81-2.077 1.19-2.653.853-.575-.336.108-1.264.568-2.071.46-.81 1.299-1.007 1.875-.67.577.337.669 1.08.21 1.888z"/></svg>
-                <p class="text-5 bold">+100</p>
+                <p class="text-5 bold">${card.uproarPoints}</p>
                 <svg xmlns="http://www.w3.org/2000/svg" class="bolt" width="18" height="18"><path fill="#FFF" d="M6.98 4.116A.206.206 0 006.795 4h-2.44L6.762.307A.195.195 0 006.77.104.207.207 0 006.589 0H3.293a.207.207 0 00-.184.11l-3.088 6a.195.195 0 00.009.195.208.208 0 00.175.095h2.117L.016 11.722a.197.197 0 00.082.249.21.21 0 00.265-.042l6.589-7.6a.196.196 0 00.029-.213z"/></svg>
-                <p class="text-5 bold">-300</p>
+                <p class="text-5 bold">${card.boltPoints}</p>
             </div>
             <div class="accept-button">
                 <button>Accept </button>
             </div>
         </div>
+    </div>
     </div>`
     
     li.innerHTML += card;
@@ -104,7 +43,7 @@ function listSmallCard(arr){
         type : 'slider',
         perView : 4,
         breakpoints: {
-            1023: {
+            1210: {
                 perView: 2
             },
             767:{
@@ -115,26 +54,12 @@ function listSmallCard(arr){
     new Glide('.glide-2', config).mount()
 }
 
-
 listSmallCard(smallCard)
 
 
-
-// const navbarMenu = document.getElementById('dropdownContentSecondary').querySelectorAll("a")
-
-// navbarMenu.forEach(menu=>{
-//     menu.addEventListener('click',function() {
-//         navbarMenu.forEach(a=>a.classList.remove('active'))
-//         this.classList.add('active')
-//          var filtererArray = smallCard.filter(card => card.genre ==`${menu.id}`);
-//          smallContainer.innerHTML = '';
-//          listSmallCard(filtererArray)
-//          console.log(filtererArray)
-//     })
-// })
-
-
 const navMenu = document.getElementById('dropdownContentSecondary').querySelectorAll("a")
+const header = document.getElementById('navbar');
+
 
 navMenu.forEach(menu=>{
     menu.addEventListener('click',function() {
@@ -146,8 +71,6 @@ navMenu.forEach(menu=>{
         smallContainer.innerHTML = '';
         listCard(filtererArray)
         listSmallCard(filtererSmallArray)
-         console.log(filtererArray)
-         console.log(filtererSmallArray)
-
+        header.classList.remove('show')
     })
 })
